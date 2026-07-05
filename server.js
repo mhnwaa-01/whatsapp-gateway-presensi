159365801139d5b9f1073c8672866dc202ac1e7f
mhnwaa-01 <dilldayy@gmail.com>
2026-07-06 06:58:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,17 +15,18 @@ let latestQr = null;
 
 const client = new Client({
     authStrategy: new LocalAuth(),
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+    },
     puppeteer: {
         headless: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process', // helps run on low memory (Render Free tier)
-            '--disable-gpu'
+            '--disable-gpu',
+            '--no-zygote'
         ],
         executablePath: process.env.CHROME_PATH || '/usr/bin/google-chrome-stable'
     }
